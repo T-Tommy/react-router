de340f3ac1da2c6e2ae51c231f3f2217d042fa73
Tommy Tran <tranttommy95@gmail.com>
2019-05-20 11:29:02 -0700

    wrote in App

---
+++ src/components/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Header from './Header';
+import Red from './Red';
+import Yellow from './Yellow';
+import Blue from './Blue';
+
+export default function App() {
+  return (
+    <Router>
+      <>
+        <Header />
+        <Switch>
+          <Route path="/red" component={ Red } />
+          <Route path="/yellow" component={ Yellow } />
+          <Route path="/blue" component={ Blue } />
+        </Switch>
+      </>
+    </Router>
+  );
+}
